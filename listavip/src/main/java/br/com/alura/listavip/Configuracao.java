673c290c2e28fa6c98d53d2b38c96f5382fe5174
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-05-15 00:23:57 -0300

    Adicionada a classe java Configuracao, atribuída a mesma um método main para nele executar as configurações da aplicação. A classe foi transformada num Controller para executar uma mensagem de boas vindas no localhost:8080, a @ResponseBody, mapeada com @RequestMapping

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,4 +1,24 @@
 package br.com.alura.listavip;
 
-public class Configuracao {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Configuracao
+{
+    @RequestMapping("/")
+    @ResponseBody
+    String ola()
+    {
+        return "Olá, bem-vindo ao sistema Lista VIPs";
+    }
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Configuracao.class, args);
+    }
 }
