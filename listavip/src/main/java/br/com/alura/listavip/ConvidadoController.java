3290956a4c9b5f65bfd160ec9c8982fa59fb10c1
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-05-17 20:18:23 -0300

    Criada a classe Controller, nela foi aidicionado o método index que mapeia a requisição em / e retorna como String o template index.html

--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -1,4 +1,14 @@
 package br.com.alura.listavip;
 
-public class ConvidadoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConvidadoController
+{
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
 }
