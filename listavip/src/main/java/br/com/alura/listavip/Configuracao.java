03909aa6437bc7b9bc0d0806c60a6a22e7db5a3d
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-05-17 20:15:39 -0300

    Alterado o codigo da classe Configuracao, retirando as anotações e imports de Controller, Request Mapping e afins que foram adicionados anteriormente para teste

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -2,21 +2,10 @@ package br.com.alura.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@Controller
 public class Configuracao
 {
-    @RequestMapping("/")
-    @ResponseBody
-    String ola()
-    {
-        return "<h1>Olá, bem-vindo ao sistema Lista VIPs<h1>";
-    }
-
     public static void main(String[] args)
     {
         SpringApplication.run(Configuracao.class, args);
