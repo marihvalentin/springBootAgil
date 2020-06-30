9c5016c97c133193a49957712b71dcf6d8c4486f
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-06-30 18:17:03 -0300

    Criado o método salvar no ConvidadoController, que recebe as informações do formulário de cadastro de novos convidados via POST

--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ConvidadoController
@@ -27,6 +28,12 @@ public class ConvidadoController
         model.addAttribute("convidados", convidados);
 
         return "listaconvidados";
+    }
+
+    @RequestMapping(value = "salvar", method = RequestMethod.POST)
+    public void salvar()
+    {
+
     }
 }
 
