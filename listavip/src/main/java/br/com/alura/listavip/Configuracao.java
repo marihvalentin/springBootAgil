9dfe27baa373069b5b10f4ed5553425166c54446
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-06-30 17:24:41 -0300

    Criação e edição de arquivos estáticos, configurado o acesso ao banco de dados, atualização de configurações, criação de classe Convidados para definição de atributos, de interface para acesso dos convidados provenientes do banco de dados e acesso deles pelo thymeleaf em sua página estática percorrendo a lista

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -2,6 +2,10 @@ package br.com.alura.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Configuracao
@@ -10,4 +14,17 @@ public class Configuracao
     {
         SpringApplication.run(Configuracao.class, args);
     }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
+        dataSource.setUsername("root");
+        dataSource.setPassword("my$qLr0o7");
+
+        return dataSource;
+    }
 }
+
--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -1,20 +1,33 @@
 package br.com.alura.listavip;
 
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ConvidadoController
 {
-    @RequestMapping("/")
-    public String index()
-    {
-        return "index";
-    }
+    @Autowired
+    private ConvidadoRepository repository;
 
-    @RequestMapping("/listaconvidados")
-    public String listaConvidados()
+    @RequestMapping("/")
+   public String index()
+   {
+       return "index";
+   }
+
+   @RequestMapping("listaconvidados")
+    public String listaConvidados(Model model)
     {
+        Iterable<Convidado> convidados = repository.findAll();
+
+        model.addAttribute("convidados", convidados);
+
         return "listaconvidados";
     }
 }
+
+
