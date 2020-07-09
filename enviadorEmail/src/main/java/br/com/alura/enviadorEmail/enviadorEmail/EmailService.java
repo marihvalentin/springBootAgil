67d236c4f45cf6ab4bd706ad4993ec6018115659
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-09 17:45:04 -0300

    Criado o projeto enviadorEmail a partir do Spring Initializr para implementação de lógica para envio de e-mails, contendo no Service os dados do servidor de email, nome e email do desinatário, email rementente e a mensagem. O projeto foi implementado separadamente para poder ser adicionado a qualquer projeto como dependência.

--- enviadorEmail/src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
+++ enviadorEmail/src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
@@ -1,4 +1,31 @@
 package br.com.alura.enviadorEmail.enviadorEmail;
 
-public class EmailService {
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService
+{
+    public void enviar(String nome, String emailDestinatario)
+    {
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("testesdevbackend@gmail.com",
+                    "12345678a!"));
+            email.setSSLOnConnect(true);
+
+            email.setFrom("testesdevbackend@gmail.com");
+            email.setSubject("Você foi convidado pelo ListaVip");
+            email.setMsg("Olá " + nome + "! Você acaba de ser convidado pelo ListaVIP.");
+            email.addTo(emailDestinatario);
+            email.send();
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+    }
 }
