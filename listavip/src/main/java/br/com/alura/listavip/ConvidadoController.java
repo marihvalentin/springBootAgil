92f30e5712f14a53aeda211d2699e4a43271884e
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-09 17:53:10 -0300

    Realizadas configurações para o envido de e-mails na Controller depois da adição da dependência, que o Maven baixou em .jar e aplicou no projeto. Criado a camada Service para refatoração de códigos pelo bem das boas práticas e adicionada a classe ConvidadoService, atualizada simultaneamente com a Controller.

--- listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ listavip/src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -1,7 +1,9 @@
 package br.com.alura.listavip;
 
+import br.com.alura.enviadorEmail.enviadorEmail.EmailService;
 import br.com.alura.listavip.model.Convidado;
 import br.com.alura.listavip.repository.ConvidadoRepository;
+import br.com.alura.listavip.service.ConvidadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ConvidadoController
 {
     @Autowired
-    private ConvidadoRepository repository;
+    private ConvidadoService service;
 
     @RequestMapping("/")
-   public String index()
+    public String index()
    {
        return "index";
    }
@@ -24,7 +26,7 @@ public class ConvidadoController
    @RequestMapping("listaconvidados")
     public String listaConvidados(Model model)
     {
-        Iterable<Convidado> convidados = repository.findAll();
+        Iterable<Convidado> convidados = service.obterTodos();
 
         model.addAttribute("convidados", convidados);
 
@@ -39,9 +41,11 @@ public class ConvidadoController
         Convidado novoConvidado = new Convidado(nome, email, telefone);
 
         //comando que salva e envia os dados preenchidos do novo convidado para o banco de dados
-        repository.save(novoConvidado);
+        service.salvar(novoConvidado);
 
-        Iterable<Convidado> convidados = repository.findAll();
+        new EmailService().enviar(nome, email);
+
+        Iterable<Convidado> convidados = service.obterTodos();
 
         model.addAttribute("convidados", convidados);
 
--- listavip/src/main/java/br/com/alura/listavip/service/ConvidadoService.java
+++ listavip/src/main/java/br/com/alura/listavip/service/ConvidadoService.java
@@ -1,4 +1,25 @@
 package br.com.alura.listavip.service;
 
-public class ConvidadoService {
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConvidadoService
+{
+    @Autowired
+    private ConvidadoRepository repository;
+
+    public Iterable<Convidado> obterTodos()
+    {
+       Iterable<Convidado> convidados =  repository.findAll();
+
+       return convidados;
+    }
+
+    public void salvar(Convidado convidado)
+    {
+        repository.save(convidado);
+    }
 }
